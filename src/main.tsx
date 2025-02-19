7f9407bd52bb161e18ca5db1a1f741d093a3591e
Imac A1418 <imaca1418@iMac-de-Imac.local>
2025-02-19 16:55:48 -0500

    Add SpeedInsights vercel

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,15 @@ import router from './router'
 import { ThemeProvider } from './components/theme-provider';
 import TopBarProgress from './components/TopBarProgress';
 import Particles from './components/Particles';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TopBarProgress />
     <Particles />
-
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
     </ThemeProvider>
+    <SpeedInsights/>
   </StrictMode>,
 )
